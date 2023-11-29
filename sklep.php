6fc52213e044cbe98ba01ce0450c3219a24b709f
Przemek <przemekchelski@gmail.com>
2023-11-29 19:58:23 +0100

    second commit

--- sklep.php
+++ sklep.php
@@ -7,53 +7,51 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    if(!empty($_POST["nazwa"]) && !empty($_POST["cena"]))
+    {
+    $conn = mysqli_connect("localhost","root","","warzywniak");
+    $query =mysqli_query($conn,"INSERT INTO Produkty (Rodzaje_id, Producenci_id, nazwa, ilosc, opis, cena, zdjecie) VALUES ((SELECT id FROM Rodzaje WHERE nazwa = 'owoce'), (SELECT id FROM Producenci WHERE nazwa = 'warzywa-rolnik'),'owoc1', 10, '', 9.99, 'owoce.jpg')");
+    mysqli_close($conn);    
+    }
+   ?>
     <div id="banerL">
         <h1>internetowy sklep z eko-warzywami</h1>             
     </div>
     <div id="banerP">
-        <ul>
+        <ol>
             <li>warzywa</li>
             <li>owoce</li>
-            <li><a href="https://terapiasokami.pl/" >Soki</a></li>
-        </ul>
+            <li><a href="https://terapiasokami.pl/"target="_blank" >Soki</a></li>
+        </ol>
     </div>
-    <div id="glowny">
+    <main>
    
         <?php
         $conn = mysqli_connect("localhost","root","","warzywniak");
       
         $query = mysqli_query($conn, "SELECT nazwa,ilosc,opis,cena,zdjecie FROM produkty WHERE Rodzaje_id = 1 OR Rodzaje_id = 2");
-        while($wynik = mysqli_fetch_row($query)){
-            echo "<div class='generowany'>
-            <img src='$wynik[4]' alt='warzywniak'>
-            <h5>$wynik[0]</h5>
-            <p>opis:$wynik[2]</p>
-            <p>na stanie:$wynik[1]</p>
-            <h2>$wynik[3] zł</h2>
-            </div>";
+        while($w = mysqli_fetch_row($query)){
+            echo '<div id="produkt">';
+            echo '<img src="' . $w[4] . '" alt="warzywniak">';
+            echo '<h5>' . $w[0] . '</h5>';
+            echo '<p>opis: ' . $w[2] . '</p>';
+            echo '<p>na stanie: ' . $w[1] . '</p>';
+            echo '<h2>' . $w[3] . 'zł';
+            echo '</div>';
         }
         mysqli_close($conn);
         ?>
         
-    </div>
+    </main>
     <footer>
-        <form action="sklep.php">
+        <form action="sklep.php" method="post">
             <input type="text" name="nazwa" id="nazwa">
             <label for="nazwa">Nazwa</label>
             <input type="text" name="cena" id="cena">
             <label for="cena">Cena</label>
             <button>Dodaj produkt</button>
         </form>
-        <?php
-            
-            if(!empty($_POST["nazwa"]) && !empty($_POST["cena"]))
-            {
-            $conn = mysqli_connect("localhost","root","","warzywniak");
-            $query =mysqli_query($conn,"INSERT INTO Produkty (Rodzaje_id, Producenci_id, nazwa, ilosc, opis, cena, zdjecie) VALUES ((SELECT id FROM Rodzaje WHERE nazwa = 'owoce'), (SELECT id FROM Producenci WHERE nazwa = 'warzywa-rolnik'),'owoc1', 10, '', 9.99, 'owoce.jpg')");
-            mysqli_close($conn);    
-            }
-           
-        ?>
         strone wykonal:0000000
     </footer>
 </body>
